e71c3c9e00177ec7d5fb7dbd96c2dca9efafead9
minwook <zkvn1103@naver.com>
2023-05-25 11:11:04 +0900

    mini-blog
    미니 블로그 완성

--- mini-blog/src/App.js
+++ mini-blog/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route }
+  from 'react-router-dom';
+import styled from 'styled-components';
+
+import MainPage from './component/page/MainPage';
+import PostWritePage from './component/page/PostWirtePage';
+import PostViewPage from './component/page/PostViewPage';
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainTitleText>민욱이의 미니 블로그</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path='post-write' element={<PostWritePage />} />
+        <Route path='post/:postId' element={<PostViewPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- mini-blog/src/component/ui/TextInput.jsx
+++ mini-blog/src/component/ui/TextInput.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledTextarea = styled.textarea`
     width: calc(100% - 32px);
-    ${props => props.height && `height: ${props.height}pxl`}
+    ${props => props.height && `height: ${props.height}px;`}
     padding: 16px;
     font-size: 16px;
     line-height: 20px;
